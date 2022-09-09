74c3d07c8193b529a53c23b85b7acbb053741e6f
KleitonAS00 <kleitonsilva.24@hotmail.com>
2022-09-09 00:45:43 -0300

    diferenciei o maior do menor

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 
 const form = document.getElementById('form-valida')
-
-function validaMaior(numeroMaior) {
-    const maior = numeroMaior
-}
+const A = document.getElementById('campoA')
+const B = document.getElementById('campoB')
 
 form.addEventListener('submit', function(e) {
     e.preventDefault()
+
+    validacao()
 })
+
+function validacao() {
+    const valorA = A.value
+    const valorB = B.value
+
+    if(valorA <= valorB) {
+        A.style.border = '2px solid red'
+    } else {
+        alert('ok')
+    }
+}
 
 console.log(form)
