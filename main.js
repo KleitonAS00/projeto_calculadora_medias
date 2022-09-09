fbf37e6fb0f927e0969159f0dbec09a6a044c21a
KleitonAS00 <kleitonsilva.24@hotmail.com>
2022-09-08 23:13:37 -0300

    Criando exercicio de validação

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const form = document.getElementById('form-valida')
+
+function validaMaior(numeroMaior) {
+    const maior = numeroMaior
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+})
+
+console.log(form)
